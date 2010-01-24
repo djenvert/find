8c8296cc66273f99d6104425327c4e021b104a34
Guillaume Sautereau <guillaumesautereau@macbook-pro-de-guillaume-sautereau.local>
2010-01-25 00:10:28 +0100

    gestion du cache

--- vendors/find_app_model.php
+++ vendors/find_app_model.php
@@ -8,6 +8,14 @@
  * Thanks to Daniel Salazar for the inspiration on pagination support
  * http://code621.com/content/10/easy-pagination-using-matt-curry-s-custom-find-types
  *
+ * Adds support for custom find methods (__findXX) and automatic caching.
+ * Automatic caching will kick in when 'cache' is passed in the $options
+ * array. 
+ * If 'cache' is a string, then it will be used to generate the
+ * cache name, which takes the format model_alias_cache_name.
+ * If 'cache' is an array, then two arguments are valid: 'name', required,
+ * and 'config', optional. 'name' is used as above, while 'config' 
+ * determines the cache configuration to use - 'default' if not specified.
  *
  * @author      Matt Curry <matt@pseudocoder.com>
  * @license     MIT
@@ -16,25 +24,31 @@
  
 class FindAppModel extends Model {
   function find($type, $options = array()) {
-    $method = null;
-    if(is_string($type)) {
-      $method = sprintf('__find%s', Inflector::camelize($type));
-    }
-    if($method && method_exists($this, $method)) {
-      $return = $this->{$method}($options);
+    $return = $this->_getCachedResults($options);
+    if (!$return) {  
+        $method = null;
+        if(is_string($type)) {
+            $method = sprintf('__find%s', Inflector::camelize($type));
+        }
+        if($method && method_exists($this, $method)) {
+            $return = $this->{$method}($options);
       
-      if($return === null && !empty($this->query['paginate'])) {
-        unset($this->query['paginate']);
-        $query = $this->query;
-        $this->query = null;
-        return $query;
-      }
-			
-			return $return;
-    } else {
-      $args = func_get_args();
-      return call_user_func_array(array('parent', 'find'), $args);
+            if($return === null && !empty($this->query['paginate'])) {
+                unset($this->query['paginate']);
+                $query = $this->query;
+                $this->query = null;
+                $return = $query;
+            }
+        } else {
+            $args = func_get_args();
+            $return = call_user_func_array(array('parent', 'find'), $args);
+        }
+        if ($this->useCache) {
+                 Cache::write($this->cacheName, $return, $this->cacheConfig);
+        }
     }
+    return $return;
+   
   }
   
   function beforeFind($query) {
@@ -52,5 +66,27 @@ class FindAppModel extends Model {
     
     return true;
   }
+  
+  function _getCachedResults($options) {
+        $this->useCache = true;
+        if (Configure::read('debug') > 0 || !isset($options['cache']) || $options['cache'] == false) {
+            $this->useCache = false;
+            return false;
+        }
+
+        if (is_string($options['cache'])) {
+            $this->cacheName = $this->alias . '_' . $options['cache'];
+        } else {
+            if (!isset($options['cache']['name'])) {
+                return false;
+            }
+            $this->cacheName = $this->alias . '_' . $options['cache']['name'];
+            $this->cacheConfig = isset($options['cache']['config']) ? $options['cache']['config'] : 'default';
+        }
+
+        $results = Cache::read($this->cacheName, $this->cacheConfig);
+
+        return $results;
+    }
 }
 ?>
